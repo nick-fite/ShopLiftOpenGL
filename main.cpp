c9e4b2b6e2aa540a1b3bc5f8f4f12f17000fbe61
nick-fite <70393136+nick-fite@users.noreply.github.com>
2025-04-11 01:44:13 -0500

    added transform, edited cmake to be more convenient

--- main.cpp
+++ main.cpp
@@ -1,10 +1,47 @@
 #include <gl/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/gtc/matrix_transform.hpp>
+#include "src/Mesh.h"
+#include "src/Transform3D.h"
+#include <vector>
 #include <iostream>
     
 using namespace std;
 
+glm::vec2 ViewportDimensions = glm::vec2(800, 600);
+glm::vec2 MousePos = glm::vec2();
+
+void resizeCallback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+    ViewportDimensions = glm::vec2(width, height);
+}
+
+
+void mouseCallback(GLFWwindow* window, double xpos, double ypos) {
+    MousePos = glm::vec2(xpos, ypos);
+}
+
 int main() {
+    glfwInit();
+
+    GLFWwindow* window = glfwCreateWindow(ViewportDimensions.x, ViewportDimensions.y, "Hello World", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    glfwSetFramebufferSizeCallback(window, resizeCallback);
+    glfwSetCursorPosCallback(window, mouseCallback);
+
+    Transform3D transform;
+    transform.SetPosition(glm::vec3(0,0,-2));
+
+    glewInit();
+
+    Mesh* mesh = new Mesh("assets/TestPlayer.fbx");
+
+    
+    
+
+
+
     cout << "hello world!" << endl;
     return 1;
 }
---
+++ src/Transform3D.h
@@ -0,0 +1,180 @@
+#include <glm/gtc/matrix_transform.hpp>
+
+//yes, header only files are not usually the best practice. However, given the nature of this tool,
+//it makes more sense to me that it's just a header. No need to overcomplicate it.
+class Transform3D {
+private: 
+    float scale;
+    glm::vec3 rotation;
+    glm::vec3 position;
+
+    bool matrixDirty;
+    bool inverseDirty;
+
+    glm::mat4 rotMatrix;
+    glm::mat4 matrix;
+    glm::mat4 inverseMatrix;
+
+public:
+    Transform3D();
+    
+    float getScale() const {
+        return scale;
+    };
+
+    glm::vec3 getRotation() const {
+        return rotation;
+    };
+
+    glm::vec3 getPosition() const {
+        return position;
+    };
+
+    void SetScale(float scale) {
+        this->scale = scale;
+        matrixDirty = true;
+    };
+
+    void SetPosition(glm::vec3 pos) {
+        position = pos;
+        matrixDirty = inverseDirty =  true;
+    };
+
+    void RotateX(float rot){
+        rotation.x += rot;
+        matrixDirty = inverseDirty =  true;
+    };
+    void RotateY(float rot){
+        rotation.y += rot;
+        matrixDirty = inverseDirty =  true;
+    };
+    void RotateZ(float rot){
+        rotation.z += rot;
+        matrixDirty = inverseDirty =  true;
+    };
+
+    void Translate(glm::vec3 vec) {
+        position += vec;
+        matrixDirty = inverseDirty =  true;
+    };
+
+    glm::mat4 GetMatrix() {
+        //recalculate matrix if it's changed
+        if(matrixDirty)
+        {
+            glm::mat4 s = glm::mat4(
+                scale, 0,0,0,
+                0, scale, 0,0,
+                0, 0, scale, 0,
+                0, 0, 0, scale
+            );
+
+            glm::mat4 rx = glm::mat4(
+                1, 0, 0, 0,
+                0, cos(rotation.x), sin(rotation.x), 0,
+                0, -sin(rotation.x), cos(rotation.x), 0,
+                0,0,0,1
+            );
+
+            glm::mat4 ry = glm::mat4(
+                cos(rotation.y), 0, sin(rotation.y), 0,
+                0, 1, 0, 0,
+                -sin(rotation.y), 0, cos(rotation.y), 0,
+                0,0,0,1
+            );
+
+            glm::mat4 rz = glm::mat4(
+                cos(rotation.z), sin(rotation.z), 0, 0,
+                -sin(rotation.z), cos(rotation.z), 0,0,
+                0,0,1,0,
+                0,0,0,1
+            );
+
+            glm::mat4 t = glm::mat4(
+                1, 0, 0, 0,
+                0, 1, 0, 0,
+                0, 0, 1, 0,
+                position.x, position.y, position.z, 1
+            );
+
+            rotMatrix = ry * rx * rz;
+            matrix = t * rotMatrix * s;
+
+            matrixDirty = false;
+
+        }
+        return matrix;
+    };
+
+
+    glm::mat4 GetInverseMatrix() {
+        if(inverseDirty) {
+            glm::mat4 s = glm::mat4(
+                1.f/scale, 0,0,0,
+                0, 1.f/scale, 0,0,
+                0, 0, 1.f/scale, 0,
+                0, 0, 0, 1.f/scale
+            );
+
+            glm::mat4 rx = glm::mat4(
+                1, 0, 0, 0,
+                0, cos(rotation.x), -sin(rotation.x), 0,
+                0, sin(rotation.x), cos(rotation.x), 0,
+                0,0,0,1
+            );
+
+            glm::mat4 ry = glm::mat4(
+                cos(rotation.y), 0, -sin(rotation.y), 0,
+                0, 1, 0, 0,
+                sin(rotation.y), 0, cos(rotation.y), 0,
+                0,0,0,1
+            );
+
+            glm::mat4 rz = glm::mat4(
+                cos(rotation.z), -sin(rotation.z), 0, 0,
+                sin(rotation.z), cos(rotation.z), 0,0,
+                0,0,1,0,
+                0,0,0,1
+            );
+
+            glm::mat4 t = glm::mat4(
+                1, 0, 0, 0,
+                0, 1, 0, 0,
+                0, 0, 1, 0,
+                -position.x, -position.y, -position.z, 1
+            );
+
+            glm::mat4 composedRot = rz * rx * ry;
+            inverseMatrix = s * composedRot * t;
+            inverseDirty = false;
+        }
+        return inverseMatrix;
+    };
+
+    glm::vec3 GetUp() {
+        if(matrixDirty) {
+            GetMatrix();
+        }
+        glm::vec4 up = rotMatrix * glm::vec4(0,1,0,1);
+
+        return glm::vec3(up);
+    };
+    
+    glm::vec3 GetForward() {
+        if(matrixDirty) {
+            GetMatrix();
+        }
+        glm::vec4 forward = rotMatrix * glm::vec4(0,0,-1,-1);
+
+        return glm::vec3(forward);
+    };
+    
+    glm::vec3 GetRight() {
+        if(matrixDirty) {
+            GetMatrix();
+        }
+        glm::vec4 right = rotMatrix * glm::vec4(1,0,0,1);
+
+        return glm::vec3(right);
+    };
+};
